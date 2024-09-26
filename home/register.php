999a86719f469af538f093e571d9f9c6f7be8928
Arunkarthik <karthikarun063@gamil.com>
2024-09-27 00:36:03 +0530

    Register.php is added and html in it is done

---
+++ home/register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koodai:Register</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="my_form">
+        <form>
+            <div class="input_deg">
+                <label>Name:</label>
+                <input type="text" name="name">
+            </div>
+            <div class="input_deg">
+                <label>Email:</label>
+                <input type="email" name="email">
+            </div>
+            <div class="input_deg">
+                <label>Phone:</label>
+                <input type="number" name="phone">
+            </div>
+            <div class="input_deg">
+                <label>Address:</label>
+                <input type="text" name="address">
+            </div>
+            <div class="input_deg">
+                <input type="submit" name="register" value="register">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
