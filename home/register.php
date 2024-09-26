162cc29d0f67fbb0d08455253534152186bfabc8
Arunkarthik <karthikarun063@gamil.com>
2024-09-27 02:23:11 +0530

    Register.php is added and php BE is done

--- home/register.php
+++ home/register.php
@@ -1,3 +1,38 @@
+<?php
+    $conn = mysqli_connect('localhost', 'root', 'root', 'php_ecom');
+
+
+    if(isset($_POST['register'])){
+        $u_name = $_POST['name'];
+        $u_email = $_POST['email'];
+        $u_phone = $_POST['phone'];
+        $u_address = $_POST['address'];
+        $u_password = $_POST['password'];
+        $usertype = "user";
+
+        $sql = "INSERT INTO users (name,email,password,phone,address,usertype) VALUES('$u_name','$u_email','$u_password','$u_phone','$u_address','$usertype')";
+
+        $data = mysqli_query($conn,$sql);
+
+        if($data)
+        {
+            echo "Registered Sucessfully";
+        }
+    }   
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +43,8 @@
 </head>
 <body>
     <div class="my_form">
-        <form>
+        <h2>Register Form:</h2>
+        <form action="" method="POST">
             <div class="input_deg">
                 <label>Name:</label>
                 <input type="text" name="name">
@@ -24,6 +60,10 @@
             <div class="input_deg">
                 <label>Address:</label>
                 <input type="text" name="address">
+            </div>
+            <div class="input_deg">
+                <label>Password:</label>
+                <input type="password" name="password">
             </div>
             <div class="input_deg">
                 <input type="submit" name="register" value="register">
